64a1f2608572154f77efb28087ae8b893f25cb2e
MichaelSu777 <msu242979@gmail.com>
2019-12-06 04:24:18 -0500

    this is the game without using the factory pattern clearly shows the need to use the factory pattern for this game

--- src/enemyshipgame/EnemyShipTesting.java
+++ src/enemyshipgame/EnemyShipTesting.java
@@ -1,5 +1,32 @@
 package enemyshipgame;
 
-public class EnemyShipTesting {
+import java.util.Scanner;
 
+//this is the game without using the factory pattern
+//clearly shows the need to use the factory pattern for this game
+public class EnemyShipTesting {
+	public static void main(String[] args) {
+		EnemyShip theEnemy = null;
+		Scanner userInput = new Scanner(System.in);
+		String enemyShipOption = "";
+		System.out.println("What type of ship? (U / R)");
+		if (userInput.hasNextLine()) {
+			enemyShipOption = userInput.nextLine();
+		}
+		if (enemyShipOption.equals("U")) {
+			theEnemy = new UFOEnemyShip();
+		} else {
+			if (enemyShipOption.equals("R")) {
+				theEnemy = new RocketEnemyShip();
+			}
+		}
+
+		doStuffEnemy(theEnemy);
+	}
+
+	public static void doStuffEnemy(EnemyShip anEnemyShip) {
+		anEnemyShip.displayEnemyShip();
+		anEnemyShip.followHeroShip();
+		anEnemyShip.enemyShipShoots();
+	}
 }
