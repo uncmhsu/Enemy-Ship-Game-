d7416725769cee96a0556d6d59e5555b24dca591
MichaelSu777 <msu242979@gmail.com>
2019-12-06 04:14:43 -0500

    finished adding everything that the enemy ship class needs

--- src/enemyshipgame/EnemyShip.java
+++ src/enemyshipgame/EnemyShip.java
@@ -2,4 +2,34 @@ package enemyshipgame;
 
 public abstract class EnemyShip {
 	private String name;
+	private double amtDamage;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getDamage() {
+		return amtDamage;
+	}
+
+	public void setDamage(double damage) {
+		amtDamage = damage;
+	}
+
+	public void followHeroShip() {
+		System.out.println(getName() + " is following the hero");
+	}
+
+	public void displayEnemyShip() {
+		System.out.println(getName() + " is on the screen");
+	}
+
+	public void enemyShipShoots() {
+		System.out.println(getName() + " attacks and does " + getDamage());
+	}
+
 }
